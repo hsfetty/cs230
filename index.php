05fbce2a4689a50163060077c2a59d99848812e2
hsfetty <hsf0004@mix.wvu.edu>
2021-02-04 22:19:21 -0500

    Adding about page

---
+++ index.php
@@ -0,0 +1,60 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+    
+    <div class="about">
+        <h1>About Us</h1>
+        <p>
+            Goodwillies serves to sell things that people cannot make it to the thrift store 
+            to access or, more often, things that no one wants. We are certain that there are 
+            people out there who are willing to pay money for these items, and we want to 
+            provide them with that opportunity.
+        </p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>Jeff Bezos</h2>
+                    <h4>CEO</h4>
+                    <p>Email: jeff@bezos.com</p>
+                    <p>Jeff recently left Amazon because he sees potential in our business. </p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Dwight K Schrute</h2>
+                    <h4>Assistant to the CEO</h4>
+                    <p>Email: assistant@tothe.ceo</p>
+                    <p>Dwight is our top salesman and a three time employee of the month. </p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpg">
+                <div class="container">
+                    <h2>Arte Fact</h2>
+                    <h4>Antique Inspector</h4>
+                    <p>Email: top@dog.net</p>
+                    <p>Arte helps us inspect and price new items. </p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Chumlee</h2>
+                    <h4>Head of Sales</h4>
+                    <p>Email: chum@lee.gov</p>
+                    <p>Chum keeps everyone in line and helps out Arte. </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
